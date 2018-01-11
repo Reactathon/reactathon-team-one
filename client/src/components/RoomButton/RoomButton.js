a1570177c9549adbcbacf0f921a56ac01963c913
Ilsa Loving <iloving@ilsa-loving.local>
2018-01-11 15:19:21 -0500

    more stuff

--- client/src/components/RoomButton/RoomButton.js
+++ client/src/components/RoomButton/RoomButton.js
@@ -7,6 +7,8 @@ import './RoomButton.css';
 class RoomButton extends React.Component {
   render() {
     const style = {
+        zIndex: 100,
+        position: "absolute",
       top: this.props.y,
       left: this.props.x,
       width: this.props.w,
