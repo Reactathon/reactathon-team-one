32b6c68f1a0565baf36461ab168617589749dc54
CryogenicBot <vikram.sharma.vik@gmail.com>
2018-01-11 15:46:13 -0500

    input form major upgrade

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,40 +1,63 @@
 import React from 'react';
 import { connect } from 'react-redux';
-<<<<<<< HEAD
 import Input, { InputLabel } from 'material-ui/Input';
 import { FormControl } from 'material-ui/Form';
 import Card, { CardActions, CardContent } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 
-
-import MapDialog from '../../components/MapDialog/MapDialog'
-=======
 import MapDialog from '../../components/MapDialog/MapDialog';
-import {addMeeting} from '../../actions/meetingActions';
->>>>>>> 36e4d5af7580df21860b5b927774006efbf32f1f
+import { addMeeting } from '../../actions/meetingActions';
 
 import './Input.css';
+
+const dateOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit'
+};
 
 class InputColumn extends React.Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   handleSubmit() {
-    this.props.addMeeting(this.props.selectedMeeting)
+    this.props.addMeeting({
+      title: this.state.title,
+      attendees: this.state.attendees,
+      organiser: "Spongebob Squarepants"
+    });
   }
 
   render() {
     const { title, start, end, attendees, room } = this.props.selectedMeeting;
-    console.log(title);
     return (
       <Card>
         <CardContent>
           <FormControl fullWidth>
             <InputLabel htmlFor="amount">Name of Meeting:</InputLabel>
+            {title !== undefined && title !== null ? (
+            <label>{title}</label>
+          ) : (
             <Input fullWidth value={title !== undefined && title !== null ? title : ''} />
+          )}
+            
           </FormControl>
 
           <label className="time-label">Time:</label>
@@ -61,7 +84,12 @@ class InputColumn extends React.Component {
 
           <FormControl fullWidth>
             <InputLabel htmlFor="attendees" className="attendees-label"># of Attendees:</InputLabel>
+            {attendees !== undefined && attendees !== null ? (
+            <label>{attendees}</label>
+          ) : (
             <Input fullWidth value={attendees !== undefined && attendees !== null ? attendees : ''} />
+          )}
+            
           </FormControl>
 
           <CardActions>
@@ -76,20 +104,41 @@ class InputColumn extends React.Component {
   }
 }
 
-//
-//   <MapDialog/>
-//
-//
-
 const mapStateToProps = state => {
   return {
-    selectedMeeting: state.selectedMeeting
+    selectedMeeting: {
+      start:
+        state.selectedMeeting.start === undefined
+          ? undefined
+          : state.selectedMeeting.start.toLocaleTimeString(
+              'en-US',
+              dateOptions
+            ),
+      end:
+        state.selectedMeeting.end === undefined
+          ? undefined
+          : state.selectedMeeting.end.toLocaleTimeString('en-US', dateOptions),
+      title:
+        state.selectedMeeting.title === undefined
+          ? undefined
+          : state.selectedMeeting.title,
+      room:
+        state.selectedMeeting.room === undefined
+          ? undefined
+          : state.selectedMeeting.room,
+      attendees:
+        state.selectedMeeting.attendees === undefined
+          ? undefined
+          : state.selectedMeeting.attendees
+    }
   };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        addMeeting: meeting => dispatch(addMeeting(meeting))
-    }
-}
-export const InputColumnLink = connect(mapStateToProps, mapDispatchToProps)(InputColumn);
+  return {
+    addMeeting: meeting => dispatch(addMeeting(meeting))
+  };
+};
+export const InputColumnLink = connect(mapStateToProps, mapDispatchToProps)(
+  InputColumn
+);
