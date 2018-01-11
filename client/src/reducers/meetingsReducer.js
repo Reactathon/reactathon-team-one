b5c6643d87ee6355a5e5c42cf79101db2972fa3b
Ilsa Loving <iloving@ilsa-loving.local>
2018-01-11 16:30:58 -0500

    cleaned up default data

--- client/src/reducers/meetingsReducer.js
+++ client/src/reducers/meetingsReducer.js
@@ -1,11 +1,11 @@
 import { ADD_MEETING } from '../actions/meetingActionTypes'
 
 const mockMeetings = [
-    {start: new Date(2018,0,10,10,0,0), end: new Date(2018,0,10,10,15,0), attendees: 'a', organiser: 't', room: 'Atrium', title: 'Meeting1'},
-    {start: new Date(2018,0,10,13,0,0), end: new Date(2018,0,10,14,0,0), attendees: 'b', organiser: 'd', room: 'Bazaar', title: 'Meeting2'},
-    {start: new Date(2018,0,10,15,0,0), end: new Date(2018,0,10,16,30,0), attendees: 'c', organiser: 'd', room: 'Void', title: 'Meeting3'},
-    {start: new Date(2018,0,11,10,0,0), end: new Date(2018,0,11,11,0,0), attendees: 'd', organiser: 'd', room: 'Turb', title: 'Meeting4'},
-    {start: new Date(2018,0,11,12,0,0), end: new Date(2018,0,11,13,0,0), attendees: 'e', organiser: 'd', room: 'Lep', title: 'Meeting5'}
+    {start: new Date(2018,0,10,10,0,0), end: new Date(2018,0,10,10,15,0), attendees: '5', organiser: 'Jessica David', room: 'Atrium', title: 'Hadoop'},
+    {start: new Date(2018,0,10,13,0,0), end: new Date(2018,0,10,14,0,0), attendees: '10', organiser: 'Dan Sarbu', room: 'Bazaar', title: 'Grilling some newbies'},
+    {start: new Date(2018,0,10,15,0,0), end: new Date(2018,0,10,16,30,0), attendees: '2', organiser: 'Matthew Solo', room: 'Forum', title: 'OTPP Status'},
+    {start: new Date(2018,0,11,10,0,0), end: new Date(2018,0,11,11,0,0), attendees: '1', organiser: 'Glenn Archer', room: 'Bazaar', title: 'Interview'},
+    {start: new Date(2018,0,11,12,0,0), end: new Date(2018,0,11,13,0,0), attendees: '4', organiser: 'Abdi', room: 'Town Hall', title: 'Route One Status'}
     ]
 
 
